3391dd95dd838bd60d2274dada8bd13bc24f4f4c
mycomputer <arjunvnd@gmail.com>
2020-02-14 19:09:15 +0530

    FIxes

--- processor/handler.js
+++ processor/handler.js
@@ -10,158 +10,95 @@ const hash = (x) => (
 const encoder = new TextEncoder('utf8');
 const decoder = new TextDecoder('utf8');
 
-const familyName = 'cookiejar';
+const familyName = 'simplestore';
 const familyVersion = '1.0';
 const namespaces = hash(familyName).substring(0, 6);
 
 
-class CJHandler extends TransactionHandler {
+let _decoder=function (payload){
+  let payload1=payload.toString().split(',')
+  let decodedval={
+    action:payload1[0],
+    value:payload1[1]
+  }
+  return decodedval
+          
+
+}
+
+
+class SimpleApp extends TransactionHandler {
     constructor() {
       super(familyName, [familyVersion], [namespaces]);
     }
-  
     apply(transactionProcessRequest, stateStore) {
-      const signerAddress = this.getAddress(transactionProcessRequest);
-      const decodedPayload = this.payloadDecoder(transactionProcessRequest.payload);
+
+     console.log('HELLO')
+    console.log('THE TRANSATION PROCESSCONTROLLER',transactionProcessRequest)
+    let usrpublickey=transactionProcessRequest.header.signerPublicKey;
+    let _hashusrpublcikkey=_hash(usrpublickey)
+    let address='a4d219'+_hashusrpublcikkey
+    
+    let update= _decoder(transactionProcessRequest.payload)
+
+    
+    console.log('THE ACTION IS',update.action,' THE VALUE IS ',update.value)
+    
+    if(update.action=='bake'){
+      let current=context.getState([address])
+
+
+      console.log('USER PUBLIC KEY',usrpublickey)
+      console.log('HASH OF USER PUBLIC KEY',_hashusrpublcikkey)
+      console.log('ADDRESS'+address)
+      console.log('CONTEXT ',current)
+      console.log('getState From the context',current)
+
+      // let newcookie=parseInt(update.value);
+      // console.log(newcookie)
+      /* .then is listens for results from a promise  */
+
+      return context.getState([address])
+        .then((stateMapping)=>{
+          console.log('The state mapping from the promise',stateMapping)
+          let myState=stateMapping[address]
+          console.log(myState)
+          
+
+          if(myState==''||myState==null ){
+            // the value of new cookies will be added to new coookie
+            var newCookie=parseInt(update.value)
+            console.log('NO of new cookies',newCookie)
+            // let oldcookie = parseInt(decoder.decode(myState)) to know if parse in works
+
+            
+
+          }else{
+            let oldCookie = parseInt(decoder.decode(myState))
+            console.log('No of old cookie is ',oldCookie)
+            //old cookie is decoded to int and is addedd to the new cookie 
+            var newCookie= parseInt(update.value)+oldCookie
+            console.log('The value currently in the state',newCookie)
+
+          }
+
+          //the total no of cookies in the currentstate is added and converted to encoded value
+          let newStateData=encoder.encode(newCookie.toString())
+
+          console.log('Data entered in encoded value',newStateData)
+          
+          let data_to_be_entered={
+            [address]:newStateData
+          }
+          return context.setState(data_to_be_entered)
+        })
+        
+        
+
+    }
+    
+    
   
-      if (decodedPayload.length === 3) {
-        const payload = {
-          action: decodedPayload[0],
-          quantity: decodedPayload[1],
-          type: decodedPayload[2],
-        };
-  
-        console.log('payload : ', payload);
-  
-        // Bake
-  
-        if (payload.action === 'bake') {
-          const hashedAddress = namespaces + hash(payload.type).substring(0, 6) + hash(signerAddress);
-          console.log('address', hashedAddress);
-          return stateStore.getState([hashedAddress])
-            .then((stateMapping) => {
-              console.log('stateMapping', stateMapping);
-              const myState = stateMapping[hashedAddress];
-              console.log('myState', myState);
-              let pastState = 0;
-              if (myState === '' || myState === null) {
-                console.log('No previous cookies, creating new cookie jar ');
-                pastState = 0;
-              } else {
-                pastState = decoder.decode(myState);
-                console.log(`Cookies in the jar:${pastState}`);
-              }
-  
-              const newStateStr = (pastState + parseInt(payload.quantity, 10)).toString();
-              const newState = encoder.encode(newStateStr);
-              const entries = {
-                [hashedAddress]: newState,
-              };
-              console.log('entries : ', entries);
-              return stateStore.setState(entries)
-                .then((returnAfterSetState) => {
-                  console.log('returnAfterSetState', returnAfterSetState);
-                  return returnAfterSetState;
-                });
-            });
-        }
-  
-        // Eat
-        if (payload.action === 'eat') {
-          const hashedAddress = namespaces + hash(payload.type).substring(0, 6) + hash(signerAddress);
-          console.log('address', hashedAddress);
-          stateStore.getState([hashedAddress])
-            .then((stateMapping) => {
-              console.log('stateMapping', stateMapping);
-              const myState = stateMapping[hashedAddress];
-              console.log('myState', myState);
-              let pastState = 0;
-  
-  
-              let newState = 0;
-              if (myState === '' || myState === null) {
-                pastState = 0;
-              } else {
-                pastState = parseInt(myState, 10);
-              }
-  
-              if (pastState < parseInt(payload.quantity, 10)) {
-                const error = new InvalidTransaction('Insufficient Balance');
-                console.log(error);
-              } else {
-                newState = pastState - parseInt(payload.quantity, 10);
-                stateStore.setState({ [hashedAddress]: newState })
-                  .then((returnAfterSetState) => {
-                    console.log('returnAfterSetState', returnAfterSetState);
-                  });
-              }
-            });
-        }
-      // eslint-disable-next-line brace-style
-      }
-  
-      // Send
-      else if (decodedPayload.length === 4) {
-        const payload = {
-          action: decodedPayload[0],
-          quantity: decodedPayload[1],
-          type: decodedPayload[2],
-          address: decodedPayload[3],
-        };
-        if (payload.action === 'send') {
-          const senderHashedAddress = namespaces
-                                    + hash(payload.type).substring(0, 6)
-                                    + hash(signerAddress);
-          const receiverHashedAddress = namespaces
-                                      + hash(payload.type).substring(0, 6)
-                                      + hash(payload.address);
-          console.log('senderAddress', senderHashedAddress);
-          stateStore.getState([senderHashedAddress])
-            .then((stateMapping) => {
-              console.log('senderStateMapping', stateMapping);
-              const senderState = stateMapping[senderHashedAddress];
-              console.log('senderState', senderState);
-              let senderPastState = 0;
-              if (senderState === '' || senderState === null) {
-                senderPastState = 0;
-              } else {
-                senderPastState = parseInt(senderState, 10);
-              }
-  
-              if (senderPastState < parseInt(payload.quantity, 10)) {
-                const error = new InvalidTransaction('Insufficient Balance');
-                console.log(error);
-              } else {
-                stateStore.getState([receiverHashedAddress])
-                  .then((receiverStateMapping) => {
-                    console.log('receiverStateMapping', receiverStateMapping);
-                    const receiverState = stateMapping[receiverHashedAddress];
-                    console.log('receiverState', receiverState);
-                    let receiverPastState = 0;
-                    if (receiverState === '' || receiverState === null) {
-                      receiverPastState = 0;
-                    } else {
-                      receiverPastState = parseInt(receiverState, 10);
-                    }
-  
-                    const senderNewState = receiverPastState + parseInt(payload.quantity, 10);
-                    const receiverNewState = senderPastState - parseInt(payload.quantity, 10);
-                    stateStore.setState({ [senderHashedAddress]: senderNewState })
-                      .then((returnAfterSetState) => {
-                        console.log('returnAfterSetState', returnAfterSetState);
-                      });
-                    stateStore.setState({ [receiverHashedAddress]: receiverNewState })
-                      .then((returnAfterSetState) => {
-                        console.log('returnAfterSetState', returnAfterSetState);
-                      });
-                  });
-              }
-            });
-        }
-      } else {
-        const error = new InvalidTransaction('Invalid Payload');
-        console.log(error);
-      }
     }
   
   
@@ -178,4 +115,4 @@ class CJHandler extends TransactionHandler {
   }
   
   
-  module.exports = CJHandler;// Module name here
+  module.exports = SimpleApp;// Module name here
--- processor/index.js
+++ processor/index.js
@@ -1,7 +1,7 @@
 const { TransactionProcessor } = require('sawtooth-sdk/processor')
 
 const SimpleStoreHandler = require('./handler')
-const transactionProcessor = new TransactionProcessor("tcp://localhost:4004")
+const transactionProcessor = new TransactionProcessor("tcp://validator:4004")
 
 
 transactionProcessor.addHandler(new SimpleStoreHandler())

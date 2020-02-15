43a11a40329d3da2f04ee4aa89536ea785e0d9c8
mycomputer <arjunvnd@gmail.com>
2020-02-15 12:36:14 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import { sendData } from "./App.service";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Test
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return ( 
+    <      div className = "App" >
+      <input type = "number" />
+      <button onClick={()=>(sendData({action:'set',value:1}))}>Set</button>
+      <button>Add</button>
+      <button>Sub</button>
+      </div>
+    );
+  }
 }
 
 export default App;
